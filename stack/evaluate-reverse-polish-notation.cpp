0d797621743d1fd3f86bec0dd48a1835de8a7697
milkyway20021012 <milkyway20021012@gmail.com>
2025-06-26 10:31:50 +0800

    LC-evaluate-reverse-polish-notation [Runtime: N/A; Memory: N/A]

---
+++ stack/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool pd(char& ch) {
+        if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            return true;
+        }
+        return false;
+    }
+
+    int evalRPN(vector<string>& tokens) { 
+        stack<int> st; 
+        for(size_t i = 0 ;i<tokens.size();++i){
+            int ret = 0;
+            if(!pd){
+                st.push(tokens[i] - '0');
+            }
+            else{
+                ret = st.top();
+                st.pop();
+                if(tokens[i] == '+'){
+                    ret = st.top() + ret;
+                    st.pop();
+                    st.push(ret);
+                }
+                else if(tokens[i] == '-'){
+                    ret = st.top() - ret;
+                    st.pop();
+                    st.push(ret);
+                }
+                else if(tokens[i] == '*'){
+                    ret = st.top() * ret;
+                    st.pop();
+                    st.push(ret);
+                }
+                else{
+                    ret = st.top() / ret;
+                    st.pop();
+                    st.push(ret);
+                }
+            }
+        }
+        return ret;
+    
+    }
+
+};
