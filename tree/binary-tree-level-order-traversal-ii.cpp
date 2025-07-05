6c9923491d26d5fd90216fb4d176decef4ec466a
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-05 11:57:55 +0800

    LC-binary-tree-level-order-traversal-ii [Runtime: N/A; Memory: N/A]

---
+++ tree/binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,33 @@
+// 先層序遍歷 再逆置
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        queue<TreeeNode*> q;
+        int levelSize = 0;
+        vector<vector<int>> vv;
+        if (root) {
+            q.push(root);
+            levelSize = 1;
+        }
+
+        while (!q.empty()) {
+            vector<int> v;
+            // 層層遍歷
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* front = q.front();
+                q.pop();
+                v.push_back(front->val);
+                if (front->left) {
+                    q.push(front->left);
+                }
+                if (front->right) {
+                    q.push(front->right);
+                }
+            }
+            levelSize = q.size();
+            vv.push_back(v);
+        }
+        reverse(vv.begin(),vv.end());
+        return vv;
+    }
+};
