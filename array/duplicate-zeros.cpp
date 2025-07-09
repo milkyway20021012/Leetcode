e017d17000be9d5458be22a954b4f4ffad79ce56
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-09 20:44:39 +0800

    LC-duplicate-zeros [Runtime: N/A; Memory: N/A]

--- array/duplicate-zeros.cpp
+++ array/duplicate-zeros.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        // 想法：先找到最後一個被覆寫的數 再從後往前進行覆寫
-        int cur = 0;
-        int dest = 0;
-
-        // 先找到最後一個複寫的數
-        while (cur < arr.size()){
-            if (arr[cur]) {
-                ++cur;
+        // 先找到最後一個要覆寫的數
+        int cur = dest = 0;
+        n = arr.size();
+        while (cur < n) {
+            if (cur) {
                 ++dest;
             } else {
-                ++cur;
                 dest += 2;
-                if (dest == arr.size() - 1 || dest == arr.size()) {
-                    break;
-                }
             }
-    }
-
-    // 判斷dest 是在什麼位置 (nums.size() -1 or nums.size() )
-    while (dest) {
-        if (dest == arr.size()) {
-            arr[dest - 1] = arr[cur];
+            if (dest >= n - 1) {
+                break;
+            }
+            ++cur;
+        }
+        // 判斷dest的情況 如果dest == arr.size() 那麼就代表說最後要覆寫的值是0
+        if (dest == n) {
+            arr[dest - 1] = 0;
             dest -= 2;
             --cur;
-        } else {
-            if (arr[cur] == 0) {
-                arr[dest] = 0;
-                arr[dest - 1] = 0;
+        }
+
+        while(cur >= 0){
+            if(arr[cur]){ arr[dest--] = arr[cur--];}
+            else{
+                arr[dest -1 ] = arr[dest] = 0;
                 --cur;
-                dest -= 2;
-                // 1,0,0,2,3,0,0,4
-            } else {
-                arr[dest] = arr[cur];
-                --dest;
-                --cur;
+                dest-=2;
             }
         }
     }
-}
-}
-;
+};
