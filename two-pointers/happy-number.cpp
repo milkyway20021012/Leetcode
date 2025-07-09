dd0e4c84b0d788d77f3721f44360924180323a22
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-09 21:28:25 +0800

    LC-happy-number [Runtime: 285 ms; Memory: 9.1 MB]

--- two-pointers/happy-number.cpp
+++ two-pointers/happy-number.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        if(n < 10 && n != 1){
-            return 0;
-        }
+        int cnt = 0;
         int val = 0;
         string str = to_string(n);
-        while(val != 1){
+        while (val != 1) {
             val = 0;
-            for(int i = 0;i<str.size();++i){
-                int num = str[i] - '0';
-                val += pow(num,2);
+            for (auto e : str) {
+                int num = e - '0';
+                val += pow(num, 2);
             }
             str = to_string(val);
+            ++cnt;
+            if (cnt >= 10000) {
+                break;
+            }
         }
-        return true;
+        if (val == 1) {
+            return true;
+        }
+        return false;
     }
 };
