b873e58b483d3435bd19c50f24640e95387e2f7a
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-09 19:27:21 +0800

    LC-move-zeroes [Runtime: 0 ms; Memory: 23.9 MB]

--- two-pointers/move-zeroes.cpp
+++ two-pointers/move-zeroes.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left = 0;
-        int right = nums.size() - 1;
-        if (left == right) {
-            return;
-        } else {
-            while (left < right) {
-                if(nums[right] == 0 && nums[left] == 0){
-                    --right;
-                }
-                if (nums[left] == 0 && nums[right] != 0 || nums[left] !=0 && nums[right]== 0) {
-                    swap(nums[left], nums[right]);
-                    ++left;
-                    --right;
-                }
-                ++left;
+        // 思路：雙指針 => dest、cur  
+        // dest 表示的是當前已經處理的非0元素
+        // cur 表示的是為處理的元素
+        // 0 - dest => 已處理 非 0
+        // dest + 1 - cur - 1 已處理的 0
+        // cur + 1 - n -1 (數組最後一個下標) => 未處理
+
+        int dest = -1; // 因爲剛開始的時候還沒有已經處理的非0元素 所以先讓dest的值不在數組下標中
+        int cur = 0;
+
+        while(cur < nums.size()){
+            if(nums[cur] == 0){
+                cur++;
             }
-            sort(nums.begin(), nums.begin() + left - 1);
+            else{
+                swap(nums[dest+1],nums[cur]);
+                cur++;
+                dest++;
+            }
         }
     }
 };
