e0257e6ae221be868e8a922ecfa417d4367c2155
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-10 08:44:24 +0800

    LC-happy-number [Runtime: N/A; Memory: N/A]

--- math/happy-number.cpp
+++ math/happy-number.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
+    int bitsum(int n){
+        int sum = 0;
+        while(n){
+            int t = n % 10;
+            sum += t * t;
+            n /=10;
+        }
+        return sum;
+    }
     bool isHappy(int n) {
-        int cnt = 0;
-        int val = 0;
-        string str = to_string(n);
-        while (val != 1) {
-            val = 0;
-            for (auto e : str) {
-                int num = e - '0';
-                val += pow(num, 2);
-            }
-            str = to_string(val);
-            ++cnt;
-            if (cnt >= 10) {
-                break;
-            }
+        // Two pointers
+        int slow = n, fast = bitsum(n);
+        while(slow != fast){
+            slow = bitsum(n);
+            fast = bitsum(bitsum(n));
         }
-        if (val == 1) {
-            return true;
-        }
-        return false;
+        return fast == 1;
+
     }
 };
