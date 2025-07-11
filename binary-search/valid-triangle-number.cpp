19b3d36269e5952448338deeffa3cbfae5aaedaf
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-11 21:08:35 +0800

    LC-valid-triangle-number [Runtime: N/A; Memory: N/A]

---
+++ binary-search/valid-triangle-number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        // 先排序
+        // 知識點： 若 a <= b <=c 則我們只要判斷 a+b是否大於c即可
+        sort(nums.begin(), nums.end());
+        int sz = nums.size()-1;
+        int max = nums[sz];
+        int left = 0;
+        int right = sz -1;
+        int ret = 0;
+        // 2 3 4 4
+        while (sz > 1) {
+            while (left != right) {
+                if (nums[left] + nums[right] > max) {
+                    ret += (right - left);
+                    --right;
+                } else {
+                    if (nums[left] + nums[right] == max) {
+                        ret += (right - left - 1); // -1 是扣掉left 自己 因爲不能等於
+                        --right;
+                    } else {
+                        ++left;
+                    }
+                }
+            }
+            left = 0;
+            max = nums[--sz];
+            right = sz - 1;
+            
+            
+        }
+        return ret;
+    }
+};
