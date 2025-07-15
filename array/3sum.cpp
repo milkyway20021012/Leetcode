5d90f1e62b029e48c2d2704a3e0fc31dbe3ae69e
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-15 20:31:45 +0800

    LC-3sum [Runtime: 45 ms; Memory: 29 MB]

--- array/3sum.cpp
+++ array/3sum.cpp
@@ -1,21 +1,51 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        // 有序數組 => 雙指針、二分查找
+
+        // 1. 先排序
         sort(nums.begin(),nums.end());
-        set<vector<int>> s;
-        for(int i = 0;i<nums.size();++i){
-            for(int j = i+1;j<nums.size();++j){
-                for(int k = j+1;k<nums.size();++k){
-                    if(nums[i] + nums[j] + nums[k] == 0){
-                        vector<int> v;
-                        v.push_back(nums[i]);
-                        v.push_back(nums[j]);
-                        v.push_back(nums[k]);
-                        s.insert(v);
+        vector<vector<int>> vv;
+        // 2. 固定數字
+        for(int i = 0; i < nums.size() - 2;){
+            // 3. 雙指針查找
+            int left = i+1;
+            int right = nums.size()-1;
+            // 固定的數字
+            int target = -nums[i];
+
+            // 雙指針開始移動
+            while(left < right){
+                if(nums[left] + nums[right] == target){
+                    vv.push_back({nums[i],nums[left],nums[right]});
+                    ++left;
+                    --right;
+                    while(left < right && nums[left] == nums[left-1] ){
+                        ++left;
+                    }
+                    while(left < right && nums[right] == nums[right+1]){
+                        --right;
+                    }
+                }
+                else{
+                    if(nums[left] + nums[right] > target){
+                        --right;
+                    }
+                    else{
+                        ++left;
                     }
                 }
             }
+            ++i;
+            while(i < nums.size() - 2){
+                if(nums[i] == nums[i-1]){
+                    ++i;
+                }
+                else{
+                    break;
+                }
+            }
         }
-        return vector<vector<int>>(s.begin(),s.end());
+        return vv;
     }
 };
