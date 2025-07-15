272de02229fb75e67d7998bf4477d84db2cda0a6
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-15 20:39:17 +0800

    LC-3sum [Runtime: 39 ms; Memory: 29 MB]

--- two-pointers/3sum.cpp
+++ two-pointers/3sum.cpp
@@ -7,14 +7,16 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>> vv;
         // 2. 固定數字
-        for(int i = 0; i < nums.size() - 2;){
+        for(int i = 0; i < nums.size();){ 
+        // 這邊i的繼續條件一定是 < nums.size()-2
+        // (因爲是三數相加，如果繼續條件不是 < nums.size() -2 會有越界問題)
             // 3. 雙指針查找
             int left = i+1;
             int right = nums.size()-1;
             // 固定的數字
             int target = -nums[i];
 
-            // 雙指針開始移動
+            // 雙指針開始移動(這裡就相當於查找兩數之和了)
             while(left < right){
                 if(nums[left] + nums[right] == target){
                     vv.push_back({nums[i],nums[left],nums[right]});
@@ -37,7 +39,7 @@ public:
                 }
             }
             ++i;
-            while(i < nums.size() - 2){
+            while(i < nums.size()){
                 if(nums[i] == nums[i-1]){
                     ++i;
                 }
