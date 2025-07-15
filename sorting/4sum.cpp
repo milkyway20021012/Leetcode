9e2e160aa0b16d39cb1453d1112dc5b5cf231fa6
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-15 21:02:34 +0800

    LC-4sum [Runtime: N/A; Memory: N/A]

---
+++ sorting/4sum.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        // 有序 => 雙指針、二分
+        // 1,0,-1,0,-2,2
+        // -2 -1 0 0 1 2
+        int n = nums.size();
+        vector<vector<int>> vv;
+        for(int i =  0 ;i <n;){ // 固定數字a (變成找三數和)
+            for (int j = i + 1;j<n;) { // 固定數字b (找雙數和)
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right) {
+                    if (nums[i] + nums[j] + nums[left] + nums[right] ==
+                        target) {
+                        vv.push_back(
+                            {nums[i], nums[j], nums[left], nums[right]});
+                        ++left;
+                        --right;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            ++left;
+                        }
+                        while (left < right && nums[right] == nums[right]) {
+                            --right;
+                        }
+                    }
+                    else{
+                        if(nums[i] + nums[j] + nums[left] + nums[right] > target){
+                            --right;
+                        }
+                        else{
+                            ++left;
+                        }
+                    }
+                }
+                ++j;
+                while(j<n && nums[j] == nums[j-1]){
+                    ++j;
+                }
+            }
+            ++i;
+            while(i < n && nums[i] == nums[i-1]){
+                ++i;
+            }
+        }
+        return vv;
+    }
+};
