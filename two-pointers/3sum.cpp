2970ce8205ebee2e83ecd69cc72576e30e822a39
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-15 20:09:20 +0800

    LC-3sum [Runtime: N/A; Memory: N/A]

--- two-pointers/3sum.cpp
+++ two-pointers/3sum.cpp
@@ -2,5 +2,20 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+        set<vector<int>> s;
+        for(int i = 0;i<nums.size();++i){
+            for(int j = i+1;j<nums.size();++j){
+                for(int k = j+1;k<nums.size();++k){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        vector<int> v;
+                        v.push_back(nums[i]);
+                        v.push_back(nums[j]);
+                        v.push_back(nums[k]);
+                        s.insert(v);
+                    }
+                }
+            }
+        }
+        return vector<vector<int>>(s.begin(),s.end());
     }
 };
