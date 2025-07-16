6e483c19ad38e2ec82c105b3841ac6f4d817389b
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-16 19:01:25 +0800

    LC-minimum-size-subarray-sum [Runtime: 7 ms; Memory: 32 MB]

--- prefix-sum/minimum-size-subarray-sum.cpp
+++ prefix-sum/minimum-size-subarray-sum.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         // Sliding Window
-        int sum = 0, ret = INT_MAX;
+        int sum = 0, len = INT_MAX;
         int n = nums.size();
-        int len = 0;
-        for(int left = 0, right = 0; right < n; right++){
+        int left,right;
+        for(left = 0, right = 0; right < n; right++){
             sum += nums[right];
             while(sum >=target){ // Update Window
-                len = right -left +1;
-                ret = min(ret,len);
+                len= min(len,right - left + 1);
                 sum -= nums[left++];
             }
         }
-        return len;
+        return len == INT_MAX ? 0 : len;
     }
 };
