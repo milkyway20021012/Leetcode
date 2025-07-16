af9031b712897d5593a8457ce623a6798f56f726
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-16 19:32:51 +0800

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // Sliding Window
+        int left = 0,right =0;
+        int ret = 0;
+        int n  = s.size();
+        int hash[128] = {0};
+        while(right < n){
+            hash[s[right]]++; // Entering Window
+            while(hash[s[right]] > 1){
+                ret = max(ret,right-left);
+                hash[s[left++]]--; // Leaving Window
+            }
+            right++;
+        }   
+        return ret;
+    }
+};
