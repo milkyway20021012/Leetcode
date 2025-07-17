ac32a719e66026a4eac92e8e78dd8837716765f5
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-17 20:06:41 +0800

    LC-minimum-operations-to-reduce-x-to-zero [Runtime: N/A; Memory: N/A]

---
+++ hash-table/minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int i,j;
+        int n = nums.size();
+       
+        int ret = INT_MAX;
+        for(i = 0;i<n;++i){
+            int sum = nums[i];
+            int count = 1;
+            for(j = i+1;j<n;++j){
+                sum += nums[j];
+                ++count;
+                // sum > x
+                if(sum > x){
+                    sum -=nums[j];
+                    --count;
+                }
+                // sum = x
+                else if(sum == x){
+                    ret = min(count,ret);
+                }
+                // sum < x
+                else{
+                    continue;
+                }
+            }
+        }
+        return ret == INT_MAX ? -1 : ret;
+    }
+};
