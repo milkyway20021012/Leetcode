f5f87ace6bb0583215e1d0d4a3eecb39092d755f
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-18 20:43:55 +0800

    LC-roman-to-integer [Runtime: N/A; Memory: N/A]

--- hash-table/roman-to-integer.cpp
+++ hash-table/roman-to-integer.cpp
@@ -2,32 +2,16 @@ class Solution {
 public:
     int romanToInt(string s) {
         int sum = 0;
-        map<string, int> m = {{"I", 1},   {"IV", 4},   {"V", 5},   {"IX", 9},
-                              {"X", 10},  {"XL", 40},  {"L", 50},  {"XC", 90},
-                              {"C", 100}, {"CD", 400}, {"D", 500}, {"CM", 900},
-                              {"M", 1000}};
-        string tmp = "";
+        map<char, int> mp{{'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
+                          {'C', 100}, {'D', 500}, {'M', 1000}};
+
         for(int i = 0;i<s.size();++i){
-            tmp += s[i];
-            if(tmp == "I" && s[i+1] == 'V' || tmp == "I" && s[i+1] == 'X'){
-                tmp += s[i+1];
-                ++i;
-                sum += m[tmp];
-            }
-            else if(tmp == "X" && s[i+1] == 'L' || tmp == "X" && s[i+1] == 'C'){
-                tmp +=s[i+1];
-                ++i;
-                sum +=m[tmp];
-            }
-            else if(tmp == "C" && s[i+1] == 'D' || tmp == "C" && s[i+1] == 'M'){
-                tmp +=s[i+1];
-                ++i;
-                sum +=m[tmp];
+            if(s[i+1] > s[i]){
+                sum -= mp[s[i]];
             }
             else{
-                sum +=m[tmp];
+                sum +=mp[s[i]];
             }
-            tmp = "";
         }
         return sum;
     }
