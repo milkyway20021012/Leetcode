f25c0b74251e06ead9d8c118bdfbdecc2e2e627e
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-18 20:53:38 +0800

    LC-roman-to-integer [Runtime: N/A; Memory: N/A]

--- hash-table/roman-to-integer.cpp
+++ hash-table/roman-to-integer.cpp
@@ -1,19 +1,26 @@
-class Solution {
 public:
-    int romanToInt(string s) {
-        int sum = 0;
-        map<char, int> mp{{'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
-                          {'C', 100}, {'D', 500}, {'M', 1000}};
-
-        for(int i = 0;i<s.size();++i){
-            if(mp[s[i+1]] > mp[s[i]]){
-                sum -= mp[s[i]];
-            }
-            else{
-                sum +=mp[s[i]];
-            }
-            cout << sum << endl;
+    int char2num(char a) {
+        switch (a) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
         }
-        return sum;
+    }
+
+    int romanToInt(string s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length() && char2num(s[i]) < char2num(s[i + 1])) {
+                result -= char2num(s[i]);
+            } else {
+                result += char2num(s[i]);
+            }
+        }
+        return result;
     }
 };
