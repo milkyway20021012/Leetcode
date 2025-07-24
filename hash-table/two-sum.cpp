b6040e2e3f0e292ebe6ced3d49ec7bc1915f9998
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-24 20:28:19 +0800

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.cpp
+++ hash-table/two-sum.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int n = numbers.size();
-        for (int k = 0; k < n * n; ++k) {
-            int i = k / n;
-            int j = k % n;
-            if (i >= j)
-                continue; // 確保每對只檢查一次，且不重複
-            if (numbers[i] + numbers[j] == target) {
-                return {i, j};
-            }
+        // hash
+        unordered_map<int,int> m;
+        for(int i = 0;i<number.size();++i){
+            int num = numbers[i];
+            if(m.find(target - num) != m.end()){
+                return {i,m[target - num]}; // 因爲題目說只有唯一解 所以找到就是答案 
+                // find => 看key return => return value
+            } 
+            m[num] = i;
+            // value = idx
         }
         return {};
     }
+
 };
