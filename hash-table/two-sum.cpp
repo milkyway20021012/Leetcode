6f90bfbf6d63671668353eeb6b3694af08108b3c
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-24 19:56:27 +0800

    LC-two-sum [Runtime: 87 ms; Memory: 13.9 MB]

--- hash-table/two-sum.cpp
+++ hash-table/two-sum.cpp
@@ -4,7 +4,7 @@ public:
         // 暴力美學
         vector<int> v;
         for(int i = 0;i<numbers.size();++i){
-            for(int j = i+1;j<number.size();++j){
+            for(int j = i+1;j<numbers.size();++j){
                 if(numbers[i] + numbers[j] == target){
                     v.push_back(i);
                     v.push_back(j);
