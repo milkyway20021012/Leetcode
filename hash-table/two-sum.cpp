32483e8d1f2b0cc3a3628d63db9a3d0c8d8376de
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-24 20:28:33 +0800

    LC-two-sum [Runtime: 0 ms; Memory: 14.9 MB]

--- hash-table/two-sum.cpp
+++ hash-table/two-sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         // hash
         unordered_map<int,int> m;
-        for(int i = 0;i<number.size();++i){
+        for(int i = 0;i<numbers.size();++i){
             int num = numbers[i];
             if(m.find(target - num) != m.end()){
                 return {i,m[target - num]}; // 因爲題目說只有唯一解 所以找到就是答案 
