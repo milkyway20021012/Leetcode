f9a01125069b11af00a95a3cf4578a7e2bb4d33d
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-24 19:57:55 +0800

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.cpp
+++ hash-table/two-sum.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         // 暴力美學
-        vector<int> v;
-        for(int i = 0;i<numbers.size();++i){
-            for(int j = i+1;j<numbers.size();++j){
+        vector<pair<int,int>> v;
+        int i,j;
+        for(i = 0;i<numbers.size();++i){
+            for(j = i+1;j<numbers.size();++j){
                 if(numbers[i] + numbers[j] == target){
-                    v.push_back(i);
-                    v.push_back(j);
                     break;
                 }
             }
         }
-        return v;
+        return {i,j};
     }
 };
