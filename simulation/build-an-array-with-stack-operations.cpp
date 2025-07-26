b329aeba70ee168234122252b90db9ec5f868afa
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-26 20:43:46 +0800

    LC-build-an-array-with-stack-operations [Runtime: 0 ms; Memory: 11 MB]

--- simulation/build-an-array-with-stack-operations.cpp
+++ simulation/build-an-array-with-stack-operations.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        stack<int> s1;
-        unordered_map<int,int> mp;
-        vector<string> v;
-        int sz = target.size() - 1 ;
-        for(auto& e : target){
-            mp[e]++;
+        stack<int> s1; // 放 [1,n] 的內容
+        stack<int> s2; // 先把 target 的內容放到 s2 中
+        vector<string> ret; // 輸出結果
+        int sz = target.size() - 1;
+        for(int i = sz;i>=0;--i){
+            s2.push(target[i]);
         }
+        cout << s2.top();
+        // n = 3 target : [1,3]
         for(int i = 1;i<=n;++i){
+            int num = s2.top();
             s1.push(i);
-            v.push_back("Push");
-            if(mp.find(i) == mp.end()){
+            ret.push_back("Push");
+            if(i != num){
                 s1.pop();
-                v.push_back("Pop");
+                ret.push_back("Pop");
+                continue;
             }
-            if(i == target[sz]){
+            s2.pop();
+            if(s2.empty()){
                 break;
             }
         }
-        return v;
+        return ret;
         
     }
 };
