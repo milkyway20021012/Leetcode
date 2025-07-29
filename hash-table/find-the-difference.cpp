cbb03b1c9d51493087ba2cb8ccff165aa23382d9
milkyway20021012 <milkyway20021012@gmail.com>
2025-07-29 09:40:03 +0800

    LC-find-the-difference [Runtime: 3 ms; Memory: 9.2 MB]

--- hash-table/find-the-difference.cpp
+++ hash-table/find-the-difference.cpp
@@ -3,7 +3,7 @@ public:
     char findTheDifference(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        for(int _left = 0;_right = 0;_left < s.size();++_left){
+        for(int _left = 0,_right = 0;_left < s.size();++_left){
             if(s[_left] != t[_right]){
                 return t[_right];
             }
