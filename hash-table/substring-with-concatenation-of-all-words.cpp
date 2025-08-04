d81e00ce88fc9ac0186cdd99e8dbf62838400298
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-04 19:09:16 +0800

    LC-substring-with-concatenation-of-all-words [Runtime: N/A; Memory: N/A]

--- hash-table/substring-with-concatenation-of-all-words.cpp
+++ hash-table/substring-with-concatenation-of-all-words.cpp
@@ -2,36 +2,34 @@ class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
         vector<int> ret;
-        unordered_map<string, int> hash1;
-        // 将words的单词丢到unordered_map中 统计频次
-        for (auto& e : words) {
-            hash1[e]++;
-        }
+        unordered_map<string, int> hash1; // 保存 words ⾥⾯所有单词的频次
+        for (auto& s : words)
+            hash1[s]++;
         int len = words[0].size(), m = words.size();
-
-        for (int i = 0; i < len; ++i) {
-            unordered_map<string, int> hash2; // 维护窗口内的单词频次
-            for (int _left = i, _right = i, count = 0; _right + len < s.size();
-                 _right += len) {
-                string in = s.substr(_right, len);
+        for (int i = 0; i < len; i++) // 执⾏ len 次
+        {
+            unordered_map<string, int> hash2; // 维护窗⼝内单词的频次
+            for (int left = i, right = i, count = 0; right + len <= s.size();
+                 right += len) {
+                // 进窗⼝ + 维护 count
+                string in = s.substr(right, len);
                 hash2[in]++;
-                // 进窗口
-                if (hash2[in] <= hash1[in])
-                    ++count;
-                // 出窗口条件
-                if (_right - _left + 1 > len * m) {
-                    string out = s.substr(_left, len);
-                    if (hash2[out] <= hash1[out])
-                        --count;
+                if (hash1.count(in) && hash2[in] <= hash1[in])
+                    count++;
+                // 判断
+                if (right - left + 1 > len * m) {
+                    // 出窗⼝ + 维护 count
+                    string out = s.substr(left, len);
+                    if (hash1.count(out) && hash2[out] <= hash1[out])
+                        count--;
                     hash2[out]--;
-                    _left += len;
+                    left += len;
                 }
                 // 更新结果
-                if (count == m) {
-                    ret.push_back(_left);
-                }
+                if (count == m)
+                    ret.push_back(left);
             }
         }
         return ret;
     }
-};
+}
