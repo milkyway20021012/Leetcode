32edbd18dfe27c79728cb00822dae18637fae567
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-06 15:28:52 +0800

    LC-multiply-strings [Runtime: 0 ms; Memory: 9.4 MB]

--- math/multiply-strings.cpp
+++ math/multiply-strings.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        if(num1 == "0" || num2 == "0"){
+        if (num1 == "0" || num2 == "0") {
             return "0";
         }
         int n1 = num1.size();
         int n2 = num2.size();
-        vector<int> v(n1+n2); // 用来存相加的结果 ( 两数相乘 最多有 n1 + n2 个位数 )
-        // 先处理相乘的结果
-        for(int i = 0;i<n1;++i){
-            for(int j = 0;j<n2;++j){
-                // 取值
+        vector<int> tmp(n1 + n2);
+        // 相加
+        for (int i = 0; i < n1; ++i) {
+            for (int j = 0; j < n2; ++j) {
                 int a = num1[n1 - i - 1] - '0';
                 int b = num2[n2 - j - 1] - '0';
-                v[i+j] += a * b;
+                tmp[i + j] += a * b;
             }
         }
-        // 处理进位
-        for(int i = 0,carry = 0;i<v.size();++i){
-            v[i] += carry;
-            carry = v[i] / 10;
-            v[i] %= 10;
+        // 處理carry
+        int carry = 0;
+        for(int i = 0;i<tmp.size();++i){
+            tmp[i] += carry;
+            carry = tmp[i] / 10;
+            tmp[i] %= 10;
         }
-        // 转回字符串
+        // 還原字串
         string ret;
-        for(int i = v.size()-1;i>=0;--i){
-            ret += v[i] + '0';
-        }
-        if(ret[0] == '0'){
-            ret.erase(ret.begin());
+        for(int i = tmp.size() - 1;i>=0;--i){
+            if(ret.empty() && tmp[i] == 0){
+                continue;
+            }
+            ret += tmp[i] + '0';
         }
         return ret;
     }
