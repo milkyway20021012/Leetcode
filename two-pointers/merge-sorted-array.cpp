4e4054138ac2e783dbd24d34c61ac088de17931c
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-06 19:29:13 +0800

    LC-merge-sorted-array [Runtime: 0 ms; Memory: 12.3 MB]

--- two-pointers/merge-sorted-array.cpp
+++ two-pointers/merge-sorted-array.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int n1 = 0,n2=0;
-        while(n1 < nums1.size() && n2 < nums2.size()){
-            if(nums1[n1] != 0 && nums1[n1] <= nums2[n2]) ++n1;
-            else if(nums1[n1] == 0){
-                nums1[n1] = nums2[n2++];
+        int end1 = m - 1;
+        int end2 = n - 1;
+        int index = m + n - 1; 
+        while(end1>=0 && end2>=0){
+            if(nums1[end1] > nums2[end2]){
+                nums1[index--] = nums1[end1--];
             }
             else{
-                ++n1;
-                nums1[n1] = nums1[n1-1];
-                nums1[n1 - 1] = nums2[n2++];
+                nums1[index--] = nums2[end2--];
             }
-
         }
-        if(m == 0){
-            for(int i = 0;i<nums1.size();++i){
-                nums1[i] = nums2[i];
-            }
+        while(end2 >= 0){
+            nums1[index--] = nums2[end2--];
         }
     }
 };
