65e5bdd5929d2b08271d64fee325b7960f0f6369
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-08 21:03:13 +0800

    LC-linked-list-cycle-ii [Runtime: N/A; Memory: N/A]

--- linked-list/linked-list-cycle-ii.cpp
+++ linked-list/linked-list-cycle-ii.cpp
@@ -13,11 +13,14 @@ public:
         ListNode* fast = head;
         while(fast && fast->next){
             fast = fast->next->next;
+            if(fast == nullptr){
+                return fast;
+            }
             if(fast == slow || fast->next == slow){
                 return slow;
             }
             slow = slow->next;
         }
-        return nullptr;
+        return fast;
     }
 };
