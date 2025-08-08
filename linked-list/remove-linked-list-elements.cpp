1760d8a1ee3886e87dd5f997bf0a2acc46c06df6
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-08 15:36:29 +0800

    LC-remove-linked-list-elements [Runtime: 0 ms; Memory: 20 MB]

---
+++ linked-list/remove-linked-list-elements.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* cur = head;
+        ListNode* prev = NULL;
+        while(cur){
+            if(cur->val == val && cur == head){
+                head = head->next;
+                cur = head;
+            }
+            else if(cur->val == val){
+                cur = cur->next;
+                prev->next = cur;
+            }
+            else{
+                prev = cur;
+                cur = cur->next;
+            }
+        }
+        return head;
+    }
+};
