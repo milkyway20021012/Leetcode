8404b51f5076a3028df36607ecc1aa1d6c77475c
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-08 20:39:34 +0800

    LC-intersection-of-two-linked-lists [Runtime: 36 ms; Memory: 21.3 MB]

--- hash-table/intersection-of-two-linked-lists.cpp
+++ hash-table/intersection-of-two-linked-lists.cpp
@@ -9,16 +9,16 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_map<ListNode*,int> um;
+        unordered_set<ListNode*> st;
 
         ListNode* cur_A = headA;
         ListNode* cur_B = headB;
         while(cur_A){
-            um[cur_A]++;
+            st.insert(cur_A);
             cur_A = cur_A->next;
         }
         while(cur_B){
-            if(um.find(cur_B) != um.end()){
+            if(st.find(cur_B) != st.end()){
                 return cur_B;
             }
             cur_B = cur_B->next;
