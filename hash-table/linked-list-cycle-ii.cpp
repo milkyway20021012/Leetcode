6b3a282eb8f0231aad8cf53e653deab98217270d
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-08 21:21:20 +0800

    LC-linked-list-cycle-ii [Runtime: N/A; Memory: N/A]

--- hash-table/linked-list-cycle-ii.cpp
+++ hash-table/linked-list-cycle-ii.cpp
@@ -8,20 +8,19 @@
  */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
+    ListNode* detectCycle(ListNode* head) {
         unordered_set<ListNode*> st;
         st.insert(head);
         ListNode* slow = head;
-        ListNode* fast = head;
-        if(slow == nullptr || slow->next == nullptr){
+        if (slow == nullptr || slow->next == nullptr) {
             return nullptr;
         }
-        while(fast){
-            fast = fast->next->next;
-            if(st.find(fast) != st.end()){
+        ListNode* fast = head->next->next;
+        while (fast) {
+            if (st.find(fast) != st.end()) {
                 return fast;
             }
-            if(st.find(fast->next) != st.end()){
+            if (st.find(fast->next) != st.end()) {
                 return fast;
             }
             slow = slow->next;
