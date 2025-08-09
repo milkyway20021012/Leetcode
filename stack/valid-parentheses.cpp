d52c2b792f5289bbe0359893b5bac5658325662c
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-09 17:28:59 +0800

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- stack/valid-parentheses.cpp
+++ stack/valid-parentheses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.size() == 1){
+        if(s.size() == 1 || s[0] == ')' || s[0] == ']' || s[0] == '}'){
             return false;
         }
         stack<char> st;
