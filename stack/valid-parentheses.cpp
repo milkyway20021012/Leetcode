1ba830bfc531986f5c4c0011e23e28c5bae31497
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-09 17:36:10 +0800

    LC-valid-parentheses [Runtime: 0 ms; Memory: 8.7 MB]

--- stack/valid-parentheses.cpp
+++ stack/valid-parentheses.cpp
@@ -2,35 +2,20 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        char top;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == ')') {
-                if(st.empty()) return false;
-                top = st.top();
-                if (top == '(') {
-                    st.pop();
-                    continue;
-                }
-                break;
-
-            } else if (s[i] == ']') {
-                if(st.empty()) return false;
-                top = st.top();
-                if (top == '[') {
-                    st.pop();
-                    continue;
-                }
-                break;
-            } else if (s[i] == '}') {
-                if(st.empty()) return false;
-                top = st.top();
-                if (top == '{') {
-                    st.pop();
-                    continue;
-                }
-                break;
-            } else {
+        for(int i = 0;i<s.size();++i){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
                 st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                else if(s[i] == ')' && st.top() == '(' || s[i] == ']' && st.top() == '[' || s[i] == '}' && st.top() == '{' ){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
             }
         }
         return st.size() == 0;
