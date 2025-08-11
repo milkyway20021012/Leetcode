0f28b30d722b739a193612c42249a7ccb6118b48
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-11 10:55:06 +0800

    LC-univalued-binary-tree [Runtime: 0 ms; Memory: 12.5 MB]

--- breadth-first-search/univalued-binary-tree.cpp
+++ breadth-first-search/univalued-binary-tree.cpp
@@ -6,29 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     bool isUnivalTree(TreeNode* root) {
-        if (root == NULL) {
-            return false;
-        }
-        int val = root->val;
-
-        if (root->left != NULL && root->left->val != val) {
-            return false;
-        }
-        if (root->right != NULL && root->right->val != val) {
-            return false;
-        }
-        if (root->left == NULL || root->right == NULL) {
+        if(root == NULL){
             return true;
         }
-        bool leftval = isUnivalTree(root->left);
-        bool rightval = isUnivalTree(root->right);
-        return leftval && rightval;
+        if(root->left && root->left->val != root->val){
+            return false;
+        }
+        if(root->right && root->right->val != root->val){
+            return false;
+        }
+        return isUnivalTree(root->left) && isUnivalTree(root->right);
     }
 };
