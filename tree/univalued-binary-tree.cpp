fe8d1e4df5081ceabe1e9b9d42608a3866de50c3
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-11 10:34:35 +0800

    LC-univalued-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ tree/univalued-binary-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        if (root == NULL) {
+            return false;
+        }
+        int val = root->val;
+        if(root->left == NULL || root->right == NULL){
+            return true;
+        }
+        if (root->left != NULL && root->left->val != val) {
+            return false;
+        }
+        if (root->right != NULL && root->right->val != val) {
+            return false;
+        }
+        bool leftval = isUnivalTree(root->left);
+        bool rightval = isUnivalTree(root->right);
+        return leftval && rightval;
+    }
+};
