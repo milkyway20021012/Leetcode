b1490b3b91030fce6e97da2a28642950761c014d
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-15 20:21:55 +0800

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: 0 ms; Memory: 17.5 MB]

--- binary-search/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ binary-search/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ret;
-        int left = 0, right = nums.size();
-        while (left <= right - 1){
+        int begin = 0;
+        if(nums.size() == 0){
+            return {-1,-1};
+        }
+        int left = 0, right = nums.size() - 1;
+        // 左區間
+        while(left < right){
             int mid = (left + right) / 2;
-            if(nums[mid] == target){
-                ret.push_back(mid);
-            }
             if(nums[mid] < target){
                 left = mid + 1;
             }
             else{
-                right = mid -1;
+                right = mid;
             }
         }
-        if(nums.size() == 1 && !ret.empty()){
-            ret.push_back(ret[0]);
-            return ret;
+        if(nums[left] != target) return {-1,-1};
+        // 先把left的結果保存下來
+        begin = left;
+        right = nums.size() -1;
+        while(left < right){
+            int mid = (left + right + 1 ) / 2;
+            if(nums[mid] > target){
+                right = mid - 1;
+            }
+            else{
+                left = mid;
+            }
         }
-        if(nums.size() != 1 && !ret.empty()){
-            ret.push_back(ret[0] + 1);
-            return ret;
-        }
-        return {-1,-1};
+        return {begin,left};
+
+
     }
 };
