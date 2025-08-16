1c279cd840464c3d796ce5f08cfa0f429547398e
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-16 19:53:54 +0800

    LC-search-insert-position [Runtime: 0 ms; Memory: 13.7 MB]

--- binary-search/search-insert-position.cpp
+++ binary-search/search-insert-position.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        if(nums.size() == 0){
-            return 0;
-        }
-        int left = 0,right = nums.size() - 1;
-        while(left < right){ 
-            int mid = ( left + right ) / 2;
-            cout << mid << endl;
-            if(nums[mid] == target || mid == 0) return mid;
-            if(nums[mid] < target){
-                left = mid + 1;
+        int left = 0, right = nums.size() - 1;
+        while(left < right){
+            int mid = (left + right + 1 ) / 2;
+            if(nums[mid] > target){
+                right = mid -1;
             }
-            else{
-                right = mid - 1;
+            else{   
+                left = mid;
             }
         }
-        cout << right << " " << nums[right];
-        if(right == 0 && nums[right] >= target) return 0;
-        return left + 1;
+        if(nums[right] > target && right != 0) return right - 1;
+        if(nums[right] < target) return right + 1;
+        return left;
     }
 };
