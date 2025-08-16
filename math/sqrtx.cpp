ac55baa0ea6c67f53783977b65c263c6d462968c
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-16 19:39:03 +0800

    LC-sqrtx [Runtime: N/A; Memory: N/A]

--- math/sqrtx.cpp
+++ math/sqrtx.cpp
@@ -4,7 +4,7 @@ public:
         if(x < 1) return 0;
         int left = 1, right = x;
         while(left < right){
-            long long mid = (left + right  + 1) / 2;
+            long long mid = (long long) (left + right  + 1) / 2;
             if(mid * mid > x) right = mid -1 ;
             else left = mid;
         }
