1b198e1726a352ea921a3b136e14b366d5163f2e
milkyway20021012 <milkyway20021012@gmail.com>
2025-08-17 19:33:28 +0800

    LC-find-minimum-in-rotated-sorted-array [Runtime: 0 ms; Memory: 14.1 MB]

--- binary-search/find-minimum-in-rotated-sorted-array.cpp
+++ binary-search/find-minimum-in-rotated-sorted-array.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int left = 0,right = nums.size() - 1;
+        // 先判斷
+        if(nums.size() == 1){
+            return nums[0];
+        }
+        int left = 0, right = nums.size() - 1;
+        // 要先判斷是否數組和未旋轉前是一樣的
+        if(nums[left] < nums[right]){
+            return nums[left];
+        }
         int x = nums[left];
         while(left < right){
-            int mid = (left + right) / 2;
-            if(nums[mid] > nums[mid + 1] || nums[mid] > x ){
+            int mid = (left + right) /2;
+            if(nums[mid] >= x){ // 代表現在在右區間
                 left = mid + 1;
             }
             else{
