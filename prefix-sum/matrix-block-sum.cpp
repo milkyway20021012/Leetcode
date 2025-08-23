10781b5832e96e1665a25626bd7a1787b2b62f3b
xyzwlbzfb <milkyway20021012@gmail.com>
2025-08-23 09:25:39 +0800

    LC-matrix-block-sum [Runtime: N/A; Memory: N/A]

--- prefix-sum/matrix-block-sum.cpp
+++ prefix-sum/matrix-block-sum.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        int m = mat.size(), n = mat[0].size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-        // 1. 预处理前缀和矩阵
-        for (int i = 1; i <= m; i++)
-            for (int j = 1; j <= n; j++)
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] +
-                           mat[i - 1][j - 1];
-        // 2. 使⽤
-        vector<vector<int>> ret(m, vector<int>(n));
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++) {
-                int x1 = max(0, i - k) + 1, y1 = max(0, j - k) + 1;
-                int x2 = min(m - 1, i + k) + 1, y2 = min(n - 1, j + k) + 1;
-                ret[i][j] = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] +
-                            dp[x1 - 1][y1 - 1];
+        int m = mat.size(),n=mat.size();
+        // 預處理
+        vector<vector<int>> dp(m+1,vector<int>(n+1));
+        for(int i = 1;i<=m;++i){
+            for(int j = 1;j<=n;++j){
+                dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + mat[i-1][j-1];
             }
+        }
+        //取結果
+        vector<vector<int>> ret(m,vector<int>(n));
+        for(int i = 0;i<m;++i){
+            for(int j = 0;j<n;++j){
+                int x1 = max(0,i-k) + 1; // +1 是因爲要符合dp
+                int y1 = max(0,j-k) + 1;
+                int x2 = min(m-1,i+k) + 1;
+                int y2 = min(n-1,j+k) + 1;
+                ret[i][j] = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1];
+            }
+        }
         return ret;
     }
 };
