f6d1027002eab7ed0afa8ded35baa76fdfd9278c
xyzwlbzfb <milkyway20021012@gmail.com>
2025-08-23 19:41:51 +0800

    LC-hamming-distance [Runtime: 0 ms; Memory: 8 MB]

--- bit-manipulation/hamming-distance.cpp
+++ bit-manipulation/hamming-distance.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0;i<=31;++i){
             int x1 = (x>>i) & 1;
             int y1 = (y>>i) & 1;
-            if(x1 != y2){
+            if(x1 != y1){
                 ret++;
             }
         }
