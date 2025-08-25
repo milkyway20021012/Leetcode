770b8080befbd182a6eeab91f3b817bc37fb38e5
xyzwlbzfb <milkyway20021012@gmail.com>
2025-08-25 19:00:16 +0800

    LC-single-number-ii [Runtime: 0 ms; Memory: 13.3 MB]

--- array/single-number-ii.cpp
+++ array/single-number-ii.cpp
@@ -2,8 +2,15 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ret = 0;
-        for(auto& e : nums){
-            ret |= e;
+        for (int i = 0; i < 32; ++i) {
+            int cnt = 0;
+            for (auto& num : nums) {
+                // 統計三的個數
+                cnt += (num >> i) & 1;
+            }
+            if(cnt % 3){
+                ret |= (1 << i);
+            }
         }
         return ret;
     }
