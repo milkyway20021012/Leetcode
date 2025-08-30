fce1be734042870ac447d63155d71932093b6bc8
xyzwlbzfb <milkyway20021012@gmail.com>
2025-08-30 19:37:56 +0800

    LC-minimum-number-of-frogs-croaking [Runtime: N/A; Memory: N/A]

--- string/minimum-number-of-frogs-croaking.cpp
+++ string/minimum-number-of-frogs-croaking.cpp
@@ -2,37 +2,35 @@ class Solution {
 public:
     int minNumberOfFrogs(string croakOfFrogs) {
         int c = 0, r = 0, o = 0, a = 0, k = 0;
-        // 最少青蛙 -> 過程中同時叫的最大數
-        int cur = 0, max_cur = 0;
-        for (auto& ch : croakOfFrogs) {
+        int current = 0, maxCurrent = 0;
+
+        for (char ch : croakOfFrogs) {
             if (ch == 'c') {
-                if (k > 0)
+                if (k > 0) {
                     --k;
-                else {
-                    ++cur;
-                    max_cur = max(cur, max_cur);
+                } else {
+                    ++current;               // 新青蛙開始叫
+                    maxCurrent = max(maxCurrent, current);
                 }
                 ++c;
             } else if (ch == 'r') {
-                if (c == 0)
-                    return -1;
-                --c, ++r;
+                if (c == 0) return -1;
+                --c; ++r;
             } else if (ch == 'o') {
-                if (r == 0)
-                    return -1;
-                --r, ++o;
+                if (r == 0) return -1;
+                --r; ++o;
             } else if (ch == 'a') {
                 if (o == 0) return -1;
-                --o,++a;
-            }
-            else{
-                if( a == 0) return -1;
-                --a,++k;
-                // 此時代表有一隻青蛙叫完了
-                --cur;
+                --o; ++a;
+            } else if (ch == 'k') {
+                if (a == 0) return -1;
+                --a; ++k;
+                --current;                  // 一隻青蛙結束叫聲
+            } else {
+                return -1; // 非法字元
             }
         }
-        if(c || r || o || a) return -1;
-        return max_cur;
+        if (c || r || o || a) return -1; // 有未完成的叫聲
+        return maxCurrent;
     }
 };
