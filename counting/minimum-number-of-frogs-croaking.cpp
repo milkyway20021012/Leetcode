4956d48cfada26616fabe9c2e5bdaa6b5f75d753
xyzwlbzfb <milkyway20021012@gmail.com>
2025-08-30 19:36:11 +0800

    LC-minimum-number-of-frogs-croaking [Runtime: N/A; Memory: N/A]

--- counting/minimum-number-of-frogs-croaking.cpp
+++ counting/minimum-number-of-frogs-croaking.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
-    int c = 0, r = 0, o = 0,a = 0,k = 0;
     int minNumberOfFrogs(string croakOfFrogs) {
-        for (int i = 0; i < croakOfFrogs.size(); ++i) {
-            if (croakOfFrogs[i] == 'c') {
-                if (k != 0) {
+        int c = 0, r = 0, o = 0, a = 0, k = 0;
+        // 最少青蛙 -> 過程中同時叫的最大數
+        int cur = 0, max_cur = 0;
+        for (auto& ch : croakOfFrogs) {
+            if (ch == 'c') {
+                if (k > 0)
                     --k;
-                    ++c;
-                } else {
-                    ++c;
+                else {
+                    ++cur;
+                    max_cur = max(cur, max_cur);
                 }
-                
-            } else if (croakOfFrogs[i] == 'r') {
-                if (c != 0) {
-                    --c;
-                    ++r;
-                }
-                else{
+                ++c;
+            } else if (ch == 'r') {
+                if (c == 0)
                     return -1;
-                }
-            } else if (croakOfFrogs[i] == 'o') {
-                if (r != 0) {
-                    --r;
-                    ++o;
-                }
-                else{
+                --c, ++r;
+            } else if (ch == 'o') {
+                if (r == 0)
                     return -1;
-                }
-            } else if (croakOfFrogs[i] == 'a') {
-                if (o != 0) {
-                    --o;
-                    ++a;
-                }
-                else{
-                    return -1;
-                }
-            } else {
-                if (a != 0) {
-                    --a;
-                    ++k;
-                }
-                else{
-                    return -1;
-                }
+                --r, ++o;
+            } else if (ch == 'a') {
+                if (o == 0) return -1;
+                --o,++a;
+            }
+            else{
+                if( a == 0) return -1;
+                --a,++k;
+                // 此時代表有一隻青蛙叫完了
+                --cur;
             }
         }
-        if(!(c || r || o || a)){
-            return k;
-        }
-        return -1;
+        if(c || r || o || a) return -1;
+        return max_cur;
     }
 };
